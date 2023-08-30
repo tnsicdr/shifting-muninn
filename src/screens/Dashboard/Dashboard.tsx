699f75462a01def57b123d61306e6868e9c41b8c
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-08-30 12:51:01 -0400

    chore: Add text

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,6 +1,14 @@
 import Card from "../../components/Card/Card";
 
 function Dashboard() {
-  return <Card>Card</Card>;
+  return (
+    <Card>
+      <div>Welcome to Shifting Muninn</div>
+      <p>
+        Shifting Muninn is an sandbox for experimenting and tinkering with web
+        development using Preact.
+      </p>
+    </Card>
+  );
 }
 export default Dashboard;
