f4e963a54c0789a0d42a6de576285bc2b02a0db8
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-08-30 12:58:18 -0400

    chore: Add layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <div className={classes.container}>
+      Shifting Muninn is powered by Preact and Vite
+    </div>
+  );
+}
+
+export default Footer;
--- src/router/root.tsx
+++ src/router/root.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "@tanstack/react-router";
 import AppBar from "../components/AppBar/AppBar";
 import classes from "./root.module.css";
+import Footer from "../components/Footer/Footer";
 
 function Root() {
   return (
-    <>
+    <div className={classes.layoutContainer}>
       <AppBar />
       <div className={classes.contentContainer}>
         <Outlet />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 export default Root;
