b9922896af4e28dd6f39b8237baf72e1da7e3b72
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-09-01 12:42:00 -0400

    chore: Style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,21 @@ import classes from "./Footer.module.css";
 function Footer() {
   return (
     <div className={classes.container}>
-      Shifting Muninn is powered by Preact and Vite
+      <div className={classes.right}>
+        <div className={classes.copyright}>
+          <div>Copyright &copy; 2023</div>
+          <div>
+            Made using{" "}
+            <a href="https://preactjs.com/" rel="noopener">
+              Preact
+            </a>{" "}
+            and{" "}
+            <a href="https://vitejs.dev/" rel="noopener">
+              Vite
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
